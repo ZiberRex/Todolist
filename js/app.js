66a9775840fd221f60af834ec5c241c4bce13f76
ZiberRex <62017222+ZiberRex@users.noreply.github.com>
2023-07-11 15:14:30 -0400

    Ultimos Cambios

--- js/app.js
+++ js/app.js
@@ -1,20 +1,18 @@
 console.log("Practica del To Do List")
-
-
-
 const formulario = document.getElementById('formulario');
 const listaTarea = document.getElementById('Lista-tareas')
-const template = document.getElementById('template')
+const input = document.getElementById('input')
+const template = document.getElementById('template').content
 const fragment = document.createDocumentFragment()
 let tareas ={
 10759938940:{
     ID:10759938940,
-    Text:'Tarea#1',
+    Text:'Tarea #1',
     estado: false
 },
 10759938941:{
     ID:10759938941,
-    Text:'Tarea#2',
+    Text:'Tarea #2',
     estado: false
 }
 }
@@ -23,7 +21,39 @@ let tareas ={
 
 formulario.addEventListener('submit', e=> {
     e.preventDefault()
-    console.log('procesando formulario...')
+   // console.log(input.value);
+
+    SetTarea(e)
 })
 
+
+const SetTarea = e =>{
+    console.log('Diste Click')
+    if(input.value.trim() === ''){
+        console.log('Vacio')
+        return
+    }
+
+    const tarea= {
+    ID: Date.now(),
+    Text: input.value,
+    estado: false
+    }
+
+    tareas[tarea.ID] = {...tarea}
+   // console.log(tareas)
+    formulario.reset();
+    input.focus();
+    pintarTareas();
+}
+
+const pintarTareas = () =>{
+    Object.values(tareas).forEach(tarea => {
+        const cloneT = template.cloneNode(true)
+        cloneT.querySelector('p').textContent= tarea.Text
+        fragment.appendChild(cloneT)
+    })
+    listaTarea.appendChild(fragment)
+
+}
  
