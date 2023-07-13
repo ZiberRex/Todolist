bfdaf30687c7aa98920e4460562c2c01c0169aea
EmilioM <62017222+ZiberRex@users.noreply.github.com>
2023-07-13 00:07:45 -0400

    Emilio
    Ultimos Cambios Todolist agrega
    Falta localstorage, limpiar y que solo agregue la faltante

--- js/app.js
+++ js/app.js
@@ -1,59 +1,56 @@
-console.log("Practica del To Do List")
-const formulario = document.getElementById('formulario');
-const listaTarea = document.getElementById('Lista-tareas')
-const input = document.getElementById('input')
-const template = document.getElementById('template').content
-const fragment = document.createDocumentFragment()
-let tareas ={
-10759938940:{
-    ID:10759938940,
-    Text:'Tarea #1',
-    estado: false
-},
-10759938941:{
-    ID:10759938941,
-    Text:'Tarea #2',
-    estado: false
-}
-}
+console.log("Practica del To Do List");
 
-//console.log(Date.now());
+        const formulario = document.getElementById('formulario');
+        const listaTarea = document.getElementById('listatareas');
+        const input = document.getElementById('input');
+        const template = document.getElementById('template').content;
+        const fragment = document.createDocumentFragment()
 
-formulario.addEventListener('submit', e=> {
-    e.preventDefault()
-   // console.log(input.value);
+        const tareas = {
+            10759938940: {
+                ID: 10759938940,
+                Text: 'Tarea #1',
+                estado: false
+            },
+            10759938941: {
+                ID: 10759938941,
+                Text: 'Tarea #2',
+                estado: false
+            }
+        };
 
-    SetTarea(e)
-})
+        formulario.addEventListener('submit', e => {
+            e.preventDefault();
+            SetTarea(e);
+            pintarTareas();
+        });
 
+        const SetTarea = e => {
+            if (input.value.trim() === '') {
+                console.log('Vacio');
+                return;
+            }
 
-const SetTarea = e =>{
-    console.log('Diste Click')
-    if(input.value.trim() === ''){
-        console.log('Vacio')
-        return
-    }
+            const tarea = {
+                ID: Date.now(),
+                Text: input.value,
+                estado: false
+            };
 
-    const tarea= {
-    ID: Date.now(),
-    Text: input.value,
-    estado: false
-    }
+            tareas[tarea.ID] = tarea;
+            formulario.reset();
+            input.focus();
+        };
 
-    tareas[tarea.ID] = {...tarea}
-   // console.log(tareas)
-    formulario.reset();
-    input.focus();
-    pintarTareas();
-}
+        const pintarTareas = () =>{
+            Object.values(tareas).forEach(tarea => {
+                console.log(tareas);
+                const cloneT = template.cloneNode(true)
+                cloneT.querySelector('p').textContent= tarea.Text
+                fragment.appendChild(cloneT)
+            })
+            listaTarea.appendChild(fragment)
+        }
+        
 
-const pintarTareas = () =>{
-    Object.values(tareas).forEach(tarea => {
-        const cloneT = template.cloneNode(true)
-        cloneT.querySelector('p').textContent= tarea.Text
-        fragment.appendChild(cloneT)
-    })
-    listaTarea.appendChild(fragment)
-
-}
- 
+        pintarTareas();
