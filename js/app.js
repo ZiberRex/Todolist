dae17741146f1fc4dd06f840a4216e5e4b42ebac
ZiberRex <62017222+ZiberRex@users.noreply.github.com>
2023-07-17 14:56:15 -0400

    emi

--- js/app.js
+++ js/app.js
@@ -6,19 +6,26 @@ console.log("Practica del To Do List");
         const template = document.getElementById('template').content;
         const fragment = document.createDocumentFragment()
 
-        const tareas = {
-            10759938940: {
-                ID: 10759938940,
-                Text: 'Tarea #1',
-                estado: false
-            },
-            10759938941: {
-                ID: 10759938941,
-                Text: 'Tarea #2',
-                estado: false
-            }
-        };
+        const tareas = {            10759938940: {
+            ID: 10759938940,
+            Text: 'Tarea #1',
+            estado: false
+        },
+        10759938941: {
+            ID: 10759938941,
+            Text: 'Tarea #2',
+            estado: false
+        }};
 
+
+        document.addEventListener('DOMContentLoaded',() => {
+            pintarTareas()
+        })
+
+
+        listaTarea.addEventListener('click', e =>{
+            btnAccion(e)
+        })
         formulario.addEventListener('submit', e => {
             e.preventDefault();
             SetTarea(e);
@@ -43,14 +50,31 @@ console.log("Practica del To Do List");
         };
 
         const pintarTareas = () =>{
+            listaTarea.innerHTML='';
             Object.values(tareas).forEach(tarea => {
                 console.log(tareas);
                 const cloneT = template.cloneNode(true)
                 cloneT.querySelector('p').textContent= tarea.Text
+                cloneT.querySelectorAll('.fas')[0].dataset.ID = tarea.ID
                 fragment.appendChild(cloneT)
             })
             listaTarea.appendChild(fragment)
         }
         
+        const btnAccion = e =>{
+            console.log(e.target.classList.contains('fa-circle-check')) 
+            if(e.target.classList.contains('fa-circle-check')){
+                tareas[e.target.dataset.ID].estado = true
+                pintarTareas()
+                console.log(tareas)
 
-        pintarTareas();
+
+            }
+            if(e.target.classList.contains('fa-circle-minus')){
+                delete tareas[e.target.dataset.ID]
+                pintarTareas()
+                console.log(tareas)
+            }
+            e.stopPropagation()
+        }
+       
